3c76f06c72ed9da1e774792c7386876efca73ceb
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2021-12-13 10:41:18 +0100

    [Feat] Updated App.java with the latest methods

--- clavager31/src/main/java/com/insa/projet4a/App.java
+++ clavager31/src/main/java/com/insa/projet4a/App.java
@@ -1,12 +1,13 @@
 package com.insa.projet4a;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -14,6 +15,88 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+
+    private static ThreadManager threadManager;
+
+    /**
+     * <p>
+     * Function called when the application is started.
+     * </p>
+     * <p>
+     * Starts a ThreadManager listening for incoming communications
+     * on port 12.
+     * </p>
+     */
+    public static void connect() {
+        threadManager = new ThreadManager(12);
+        threadManager.startServer();
+        System.out.println("Waiting for connexion on port 12");
+        System.out.println();
+    }
+
+    /**
+     * Creates a ClientThread to send messages to {@code receivAddress}
+     * 
+     * @param receivAddress Address of the destinary
+     * @return {@code True} if the establishment of the discussion was successful.
+     *         False
+     *         otherwise
+     */
+    public static boolean newDiscussion(InetAddress receivAddress) {
+        try {
+            threadManager.createClientThread(12, receivAddress);
+            return true;
+        } catch (IOException e) {
+            System.err.println("Failed to establish connexion with target");
+            return false;
+        }
+    }
+
+    /**
+     * Display a received message
+     * TODO #3 Change to call the GUI
+     * 
+     * @param msg     Message received
+     * @param address Address of the sender
+     */
+    public static void displayMsg(String msg, InetAddress address) {
+        System.out.println("Msg received from " + address + " --- " + msg);
+    }
+
+    /**
+     * Called by the Thread Manager to notify the Application that {@code address}
+     * closed the connection
+     * <p>
+     * TODO #2 Change to call the GUI
+     * 
+     * @param address address of the remote client
+     */
+    public static void notifyConnectionClosed(InetAddress address) {
+        System.out.println("Connection closed by remote initiative with " + address);
+        // TODO #1 Integration with GUI - Remove the user from ongoing connections list
+    }
+
+    /**
+     * Transmits a message to another user
+     * 
+     * @param msg           Message transmitted
+     * @param receivAddress Address of the user
+     */
+    public static void transmitMessage(String msg, InetAddress receivAddress) {
+        threadManager.transmitMessage(msg, receivAddress);
+    }
+
+    /**
+     * <p>
+     * Ends the discussion with a specified address{@code receivAddress}
+     * </p>
+     * 
+     * @param receivAddress
+     */
+    public static void endDiscussion(InetAddress receivAddress) {
+        ThreadManager.closeConnectionThreads(receivAddress);
+        System.out.println("Connexion closed by local initiative with " + receivAddress);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
