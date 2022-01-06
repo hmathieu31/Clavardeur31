8890a78c24dbcb217d49807ce518ef7840332f2c
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2022-01-06 17:05:40 +0100

    CLAV-49: 🚀 Add Main class for deployment

---
+++ src/main/java/com/insa/projet4a/Main.java
@@ -0,0 +1,9 @@
+package com.insa.projet4a;
+
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException, InterruptedException {
+        App.main(args);
+    }
+}
