d11f33e54429bc12431c83822f21ea5f0f79b3bc
HeineKayn <thomashoger@hotmail.fr>
2022-01-12 16:32:38 +0100

    junit bdd

--- src/test/java/com/insa/projet4a/bddTest.java
+++ src/test/java/com/insa/projet4a/bddTest.java
@@ -2,23 +2,48 @@ package com.insa.projet4a;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class bddTest {
     
-    static BDDManager bdd;
+    static BDDManager db;
 	
 	@BeforeAll
 	static void init() {
-		bdd = new BDDManager("test");
+		db = new BDDManager("test");
 	} 
 
     @Test
     void testConnect() throws SQLException{
-        String url = "jdbc:sqlite:src/main/resources/com/insa/projet4a/sqlite/db/test.db";
+        String url = "jdbc:sqlite:test.db";
         DriverManager.getConnection(url);
         assert true;
+    }
+
+    @Test
+    void testInit() throws SQLException{
+        db.initHistory();
+    }
+
+
+    @Test
+    void testInsert() throws SQLException{
+        db.insertHistory("test", false, "content", "date");
+    }
+
+    @Test
+    void testHistory() throws SQLException{
+        ArrayList<Message> list = db.showHistory();
+        for (Message message : list) {
+            System.out.println(message);
+        }
+    }
+
+    @Test
+    void testRemove() throws SQLException{
+        db.clearHistory("test");
     }
 }
